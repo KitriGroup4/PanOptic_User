227bb653d72dec996efcb77ac9a427c6de1884d8
ckdtj <ckdtj@CS_NOTE_2>
2017-05-18 10:06:42 +0900

    user login

---
+++ user_program/src/com/kitri/user/login/UserLoginListener.java
@@ -0,0 +1,41 @@
+package com.kitri.user.login;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.kitri.user.main.Main;
+import com.kitri.user.network.PacketInformation;
+
+
+public class UserLoginListener implements ActionListener {
+
+    UserLogin view;
+    // UserLoginLogic logic;
+
+    public UserLoginListener(UserLogin view) {
+	this.view = view;
+	// logic = new UserLoginLogic(view);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	Object o = e.getSource();
+	if (o == view.joinBtn) {
+	    // logic.viewJoin();
+	    view.join.setVisible(true);
+	}
+	if (o == view.loginBtn) {
+	    login();
+	}
+    }
+
+    public void login() {
+	String id = view.idTf.getText().trim();
+	String pw = view.pwTf.getText().trim();
+
+	// 유효성 검사
+
+	Main.network.sendPacket(PacketInformation.Operation.LOGIN, PacketInformation.PacketType.ID_PW, id + "," + pw);
+
+    }
+}
